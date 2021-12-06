9d87506d1d9399afa37a48901cf75812e78c7e4a
Ingmar Kole <ikole@student.codam.nl>
2021-12-06 11:47:32 +0100

    ✔️ Performance: significantly improved performance pog

--- d06/d06.py
+++ d06/d06.py
@@ -26,22 +26,32 @@
 #                                                                              #
 # **************************************************************************** #
 	
-fish = [int(x) for x in open("d06.input").read().split(",")]
+school = [int(x) for x in open("d06.input").read().split(",")]
 
-def fish_things(fish):
-	if fish == 0:
-		fish == 7
-		fish.append(8)
-	fish -= 1
+def init_fishes(population):
+	for fish in school:
+		population[fish] += 1
+
+def increase_population(population):
+	spawn = population[0]
+	for i in range(0, len(population) - 1):
+		population[i] = population[i + 1]
+	population[6] += spawn
+	population[8] = spawn
+
+def calculate_population_size(population):
+	size = 0
+	for i in range(len(population)):
+		size += population[i]
+	return size
 
 def simulate_fish(ex, amount):
+	population = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0}
+	init_fishes(population)
 	for days in range(amount):
-		for i in range(len(fish)):
-			if fish[i] == 0:
-				fish[i] = 7
-				fish.append(8)
-			fish[i] -= 1
-	print(ex, len(fish))
+		increase_population(population)
+	calculate_population_size(population)
+	print(ex, calculate_population_size(population))
 
 simulate_fish("ex00:", 80)
 simulate_fish("ex01:", 256)
