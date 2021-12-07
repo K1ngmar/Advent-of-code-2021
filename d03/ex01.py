4b07c6060234625e94d425fa598f4747b386aa0a
Ingmar Kole <ikole@student.codam.nl>
2021-12-08 00:43:47 +0100

    😅 Chore: changed variable naming

--- d03/ex01.py
+++ d03/ex01.py
@@ -30,10 +30,10 @@ lines = open("d03.input").read().split()
 l2 = lines.copy()
 
 for i in range(len(lines[0])):
-	fuck = str(int(sum(map((lambda x:x[i]=="1"), lines)) >= len(lines) / 2))
-	lines = [l for l in lines if l[i] == fuck]
+	b1 = str(int(sum(map((lambda x:x[i]=="1"), lines)) >= len(lines) / 2))
+	lines = [l for l in lines if l[i] == b1]
 	if len(l2) > 1:
-		no = str(int(sum(map((lambda x:x[i]=="1"), l2)) >= len(l2) / 2))
-		l2 = [l for l in l2 if l[i] != no]
+		b2 = str(int(sum(map((lambda x:x[i]=="1"), l2)) >= len(l2) / 2))
+		l2 = [l for l in l2 if l[i] != b2]
 
 print(int(lines[0], 2) * int(l2[0], 2))
