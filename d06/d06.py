35885cd80c590d3c6d0d44fedc37963e3221f37e
Ingmar Kole <ikole@student.codam.nl>
2021-12-07 21:27:10 +0100

    m🏌️Succesfully golfed another day

--- d06/d06.py
+++ d06/d06.py
@@ -26,32 +26,11 @@
 #                                                                              #
 # **************************************************************************** #
 	
-school = [int(x) for x in open("d06.input").read().split(",")]
-
-def init_fishes(population):
-	for fish in school:
-		population[fish] += 1
-
-def increase_population(population):
-	spawn = population[0]
-	for i in range(0, len(population) - 1):
-		population[i] = population[i + 1]
-	population[6] += spawn
-	population[8] = spawn
-
-def calculate_population_size(population):
-	size = 0
-	for i in range(len(population)):
-		size += population[i]
-	return size
-
-def simulate_fish(ex, amount):
-	population = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0}
-	init_fishes(population)
-	for days in range(amount):
-		increase_population(population)
-	calculate_population_size(population)
-	print(ex, calculate_population_size(population))
-
-simulate_fish("ex00:", 80)
-simulate_fish("ex01:", 256)
+def s(a):
+	p=[]
+	for i in range(9):p.append(f.count(i))
+	for _ in range(a):p=p[1:]+p[:1];p[6]+=p[8]
+	print(sum(p))
+f = [int(x) for x in open("i").read().split(",")]
+s(80)
+s(256)
